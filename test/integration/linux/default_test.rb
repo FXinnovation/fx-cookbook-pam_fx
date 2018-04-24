13da8be4e6e5c0f805a7f1046136f2492bbff280
Cabillot Julien <julien.cabillot@fxinnovation.com>
2018-04-24 10:59:13 -0400

    add inspec test

--- test/integration/linux/default_test.rb
+++ test/integration/linux/default_test.rb
@@ -5,14 +5,15 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+control "pam_fx - #{os.name} #{os.release} - 01" do
+  title 'Ensure PAM file are correct'
+  describe file('/etc/pam.d/pam-test') do
     it { should exist }
+    its('group')   { should eq 'root' }
+    its('owner')   { should eq 'root' }
+    its('type')    { should eq :file }
+    its('mode')    { should cmp '0644' }
+    its('content') { should match(/^auth[[:space:]]*subtask[[:space:]]*password-auth[[:space:]]*$/) }
+    its('content') { should match(/^session[[:space:]]*required[[:space:]]*pam_selinux.so[[:space:]]*open env_params$/) }
   end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
 end
