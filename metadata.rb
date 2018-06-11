5d18449fc7b503b0a729bc9a6dc4f47fb2b6484a
Cabillot Julien <julien.cabillot@fxinnovation.com>
2018-06-11 15:04:31 -0400

    pam_fx doesn't supports windows, obviously

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,11 @@ maintainer       'FXinnovation'
 maintainer_email 'cloudsquad@fxinnovation.com'
 description      'Provide ressources to configure PAM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 chef_version     '>= 12.14' if respond_to?(:chef_version)
 issues_url       'https://bitbucket.org/fxadmin/public-common-cookbook-pam_fx/issues'
 source_url       'https://bitbucket.org/fxadmin/public-common-cookbook-pam_fx'
 supports         'redhat',  '>= 7.0'
 supports         'centos',  '>= 7.0'
-supports         'windows', '>= 6.1'
 supports         'ubuntu',  '>= 16.04'
 supports         'debian',  '>= 8.0'
